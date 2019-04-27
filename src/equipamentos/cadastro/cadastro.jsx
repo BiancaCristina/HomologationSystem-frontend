b59b1505116b499647b1274d00aff1bd8462ccd1
Bianca <biancacristina-silva@outlook.com>
2019-04-27 18:51:46 -0300

    Created tabs

--- src/equipamentos/cadastro/cadastro.jsx
+++ src/equipamentos/cadastro/cadastro.jsx
@@ -1,17 +1,36 @@
 import React, { Component } from 'react'
+
 import ContentHeader from '../../common/template/contentHeader'
 import Content from '../../common/template/content'
+import Tabs from '../../common/tab/tabs'
+import TabsHeader from '../../common/tab/tabsHeader'
+import TabsContent from '../../common/tab/tabsContent'
+import TabHeader from '../../common/tab/tabHeader'
 
 class Cadastro extends Component {
-  render() {
-    return (
-      <div>
-        <ContentHeader title='Equipamentos' small='Cadastro' />
-        <Content>
-          Equipamentos
-        </Content>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+
+			<ContentHeader title='Equipamentos' small='Cadastro' />
+
+			<Content>
+				<Tabs>
+					<TabsHeader>
+						<TabHeader label='' icon='plus' target='tabCreate' />
+						<TabHeader label='' icon='pencil' target='tabUpdate' />
+						<TabHeader label='' icon='trash-o' target='tabDelete' />
+					</TabsHeader>
+
+					<TabsContent>
+
+					</TabsContent>
+				</Tabs>
+			</Content>
+
+			</div>
+		)
+	}
 }
+
 export default Cadastro
--- src/equipamentos/consulta/consulta.jsx
+++ src/equipamentos/consulta/consulta.jsx
@@ -1,17 +1,34 @@
 import React, { Component } from 'react'
+
 import ContentHeader from '../../common/template/contentHeader'
 import Content from '../../common/template/content'
+import Tabs from '../../common/tab/tabs'
+import TabsHeader from '../../common/tab/tabsHeader'
+import TabsContent from '../../common/tab/tabsContent'
+import TabHeader from '../../common/tab/tabHeader'
+
 
 class Consulta extends Component {
-  render() {
-    return (
-      <div>
-        <ContentHeader title='Equipamentos' small='Consulta' />
-        <Content>
-          Equipamentos
-        </Content>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+
+			<ContentHeader title='Equipamentos' small='Consulta' />
+
+			<Content>
+				<Tabs>
+					<TabsHeader>
+						<TabHeader label='' icon='bars' target='tabList' />
+					</TabsHeader>
+
+					<TabsContent>
+
+					</TabsContent>
+				</Tabs>
+			</Content>
+
+			</div>
+		)
+	}
 }
 export default Consulta

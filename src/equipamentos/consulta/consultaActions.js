89c19c523f38b940e65647afcaaa3d1dc515eb37
Bianca <biancacristina-silva@outlook.com>
2019-04-28 17:36:10 -0300

    Created state that shows equipamentos paginados

--- src/equipamentos/consulta/consultaActions.js
+++ src/equipamentos/consulta/consultaActions.js
@@ -3,7 +3,7 @@ const BASE_URL = 'http://localhost:8080/equipamentos'
 
 export function getList() {
     const request = axios.get(`${BASE_URL}/page`)
-    
+
     return {
         type: 'EQUIPAMENTOS_FETCHED',
         payload: request
--- src/equipamentos/consulta/consultaPaginado.jsx
+++ src/equipamentos/consulta/consultaPaginado.jsx
@@ -5,36 +5,56 @@ import { getList } from './consultaActions'
 
 class ConsultaPaginado extends Component {
 
-    componentWillMount() {
-        this.props.getList()
+    constructor(props) {
+        super(props)
+        this.state = {
+            equipamentos: []};
+    }
+
+    componentDidMount() {
+        // Faz um GET no endpoint que retorna os equipamentos paginados
+        const BASE_URL = 'http://localhost:8080/equipamentos'
+
+        fetch(`${BASE_URL}/page`)
+        .then(response => response.json())
+        .then(data => this.setState({equipamentos: data.content}));
     }
 
     render() {
-        console.log(this.props.getList())
-        
+        // Pega o estado do componente e usa pra recuperar os dados do backend
+        const {equipamentos} = this.state;
+        equipamentos && console.log(equipamentos)
+
         return (
             <div>
                 <table className='table'>
                     <thead>
-                        <tr>
-                            <th>R12</th>
-                            <th>Nome</th>
-                            <th>Fabricante</th>
-                            <th>Descrição</th>
-                            <th>Status</th>
-                            <th>Data</th>
-                        </tr>
+                        <th>R12</th>
+                        <th>Nome</th>
+                        <th>Fabricante</th>
+                        <th>Descrição</th>
+                        <th>Status</th>
+                        <th>Data</th>
+
                     </thead>
 
                     <tbody>
-
+                        { equipamentos && equipamentos.map(
+                            item => 
+                                <tr key={item.id} >
+                                    <td>{item.r12}</td>
+                                    <td>{item.nome}</td>
+                                    <td>{item.fabricante}</td>
+                                    <td>{item.descricao}</td>
+                                    <td>{item.status}</td>
+                                    <td>{item.dataUltimaEdicao}</td>
+                                </tr>
+                        )}
                     </tbody>
                 </table>
             </div>
-        )
+        );
     }
 }
 
-const mapStateToProps = state => ({list: state.consulta.list})
-const mapDispatchToProps = dispatch => bindActionCreators({getList}, dispatch)
-export default connect(mapStateToProps, mapDispatchToProps)(ConsultaPaginado)
+export default ConsultaPaginado

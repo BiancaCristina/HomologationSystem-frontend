4458e288e805b53aa960621410e2bf9ff1518120
Bianca <biancacristina-silva@outlook.com>
2019-05-07 11:04:16 -0300

    Implemented resetForm

--- src/equipamentos/cadastro/cadastroActions.js
+++ src/equipamentos/cadastro/cadastroActions.js
@@ -1,18 +1,21 @@
 import axios from 'axios'
 import { toastr } from 'react-redux-toastr'
+import { reset as resetForm } from 'redux-form'
 
 const BASE_URL = 'http://localhost:8080/equipamentos'
 
 export function create(values) {
-    axios.post(`${BASE_URL}`, values)
-    .then(resp => {
-            toastr.success('Sucesso', 'Equipamento cadastrado')
-        })
-        .catch(e => {
-            toastr.error('Erro', 'Houve problemas para cadastrar equipamento')
-            //e.response.data.errors.forEach(error => toastr.error('Erro', error))
-        })
-    return {
-        type: 'TEMP'
+    return dispatch => {
+        axios.post(`${BASE_URL}`, values)
+            .then(resp => {
+                toastr.success('Sucesso', 'Equipamento cadastrado')
+                dispatch([
+                    resetForm('CadastroForm')
+                ])
+            })
+            .catch(e => {
+                toastr.error('Erro', 'Houve problemas para cadastrar equipamento')
+                //e.response.data.errors.forEach(error => toastr.error('Erro', error))
+            })
     }
 }

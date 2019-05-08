3e95a457a6e679bcb36ad552832bb79ca980ee0b
Bianca <biancacristina-silva@outlook.com>
2019-05-08 15:49:50 -0300

    Implemented pagination

--- src/equipamentos/consulta/consultaPaginado.jsx
+++ src/equipamentos/consulta/consultaPaginado.jsx
@@ -1,7 +1,8 @@
 import React, {Component} from 'react'
-import { bindActionCreators } from 'redux'
+import { bindActionCreators } from 'redux' 
 import { connect } from 'react-redux'
 import { getList } from './consultaActions'
+import Pagination from '../../common/pagination/pagination'
 
 function searchingFor(termo) {
     // Verifica se a string em "term" está inclusa dentro do filtro nome
@@ -17,23 +18,35 @@ function searchingFor(termo) {
 class ConsultaPaginado extends Component {
 
     constructor(props) {
-        super(props)
-
+        super(props);
         // O estado "term" define o filtro de busca
 
         this.state = {
             equipamentos: [],
-            termo: ''};
+            termo: '',
+            pageOfItems: []};
+
         this.handleSearch = this.handleSearch.bind(this);
+        this.onChangePage = this.onChangePage.bind(this);
+/* 
+        const BASE_URL = 'http://localhost:8080/equipamentos'
+
+        fetch(`${BASE_URL}/page`)
+        .then(response => response.json())
+        .then(data => this.setState({equipamentos: data.content})); */
     }
 
-    componentDidMount() {
+    componentWillMount() {
         // Faz um GET no endpoint que retorna os equipamentos paginados
         const BASE_URL = 'http://localhost:8080/equipamentos'
 
         fetch(`${BASE_URL}/page`)
         .then(response => response.json())
         .then(data => this.setState({equipamentos: data.content}));
+    }
+
+    onChangePage(pageOfItems) {
+        this.setState({pageOfItems: pageOfItems})
     }
 
     handleSearch(event) {
@@ -50,10 +63,10 @@ class ConsultaPaginado extends Component {
                 <div role='form' className='todoForm'>
                     <div className='col-xs-12 col-sm-9 col-md-10'>
                         <input id='description' className='form-control'
-                        placeholder='Pesquise um equipamento'
-                        onChange={this.handleSearch}
-                        value={termo}
-                        ></input>
+                            placeholder='Pesquise um equipamento'
+                            onChange={this.handleSearch}
+                            value={termo}
+                        />
                     </div>
                 </div>
 
@@ -68,19 +81,20 @@ class ConsultaPaginado extends Component {
 
                     </thead>
 
-                    <tbody>
-                        { equipamentos && equipamentos.filter(searchingFor(termo)).map(
-                            item => 
-                                <tr key={item.id} >
-                                    <td>{item.r12}</td>
-                                    <td>{item.nome}</td>
-                                    <td>{item.fabricante}</td>
-                                    <td>{item.descricao}</td>
-                                    <td>{item.status}</td>
-                                    <td>{item.dataUltimaEdicao}</td>
-                                </tr>
+                    <tbody className="container">
+                        {this.state.pageOfItems.filter(searchingFor(termo)).map(item =>
+                            <tr key={item.id}>
+                                <td>{item.r12}</td>
+                                <td>{item.nome}</td>
+                                <td>{item.fabricante}</td>
+                                <td>{item.descricao}</td>
+                                <td>{item.status}</td>
+                                <td>{item.dataUltimaEdicao}</td>
+                            </tr>
                         )}
-                    </tbody>
+                        <Pagination items={this.state.equipamentos} onChangePage={this.onChangePage} />
+                    </tbody> 
+                    
                 </table>
             </div>
         );
